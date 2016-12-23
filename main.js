1e49c147d78780ecefca263bbab54612a1b221f2
Jonathan Commins <joncom@gmail.com>
2016-12-23 01:17:08 -0800

    First Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+['audio1.ogg', 'audio2.ogg', 'audio3.ogg'].forEach((path) => {
+	var audio = new Audio(path);
+	audio.addEventListener('canplaythrough', () => { 
+		console.log('"Loaded"');
+	}, false);
+});
