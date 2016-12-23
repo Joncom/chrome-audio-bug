f153708b7365164f9efe0357d954e3883de7cc66
Jonathan Commins <joncom@gmail.com>
2016-12-23 02:05:04 -0800

    Simplify audio loading

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 ['audio1.ogg', 'audio2.ogg', 'audio3.ogg'].forEach((path) => {
-	var audio = new Audio(path);
-	audio.addEventListener('canplaythrough', () => { 
-		console.log('"Loaded"');
-	}, false);
+	new Audio(path);
 });
