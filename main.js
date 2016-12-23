525182a334a04fcbe71bb5c198148fd184620b82
Jonathan Commins <joncom@gmail.com>
2016-12-23 10:31:24 -0800

    Add "Loaded OK" message to event callback

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 ['audio1.ogg', 'audio2.ogg', 'audio3.ogg'].forEach((path) => {
-	new Audio(path);
+	var audio = new Audio(path);
+	audio.addEventListener('canplaythrough', () => { 
+		console.log('Loaded OK');
+	}, false);
 });
